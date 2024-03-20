2d0d05aee0b7e57907813c1f8dd6db20065c21b2
sura <45725129+surashirishi@users.noreply.github.com>
2024-03-20 13:14:41 +0900

    Update entrypoint.sh

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -l
 
-./config.sh --url ./config.sh --url $REPO_URL --token "BHCAUWD46GKUKFXMSFCEBTTF7JWFBAVPNFXHG5DBNRWGC5DJN5XF62LEZYBOK3VFWFUW443UMFWGYYLUNFXW4X3UPFYGLN2JNZ2GKZ3SMF2GS33OJFXHG5DBNRWGC5DJN5XA" --unattended --ephemeral && ./run.sh
+# Retrieve a short lived runner registration token using the PAT
+REGISTRATION_TOKEN="$(curl -X POST -fsSL \
+  -H 'Accept: application/vnd.github.v3+json' \
+  -H "Authorization: Bearer $GITHUB_PAT" \
+  -H 'X-GitHub-Api-Version: 2022-11-28' \
+  "$REGISTRATION_TOKEN_API_URL" \
+  | jq -r '.token')"
+
+./config.sh --url $REPO_URL --token $REGISTRATION_TOKEN --unattended --ephemeral && ./run.sh
