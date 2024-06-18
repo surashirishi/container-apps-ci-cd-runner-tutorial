be2c482480097e85c0db6a4d9f1c450be9e212cd
sura <45725129+surashirishi@users.noreply.github.com>
2024-06-18 17:28:22 +0900

    Update entrypoint.sh

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 # 以下の環境変数を利用して処理を行います。事前に container app ジョブ側の環境変数の設定が必要です。
 # $PEM_KEY
@@ -33,24 +33,43 @@ echo "$PEM_KEY" > github_app_private_key.pem
 # echo "below is file github_app_private_key.pem content."
 # cat ./github_app_private_key.pem
 
-echo "start original sh yes."
+# try-bash
+set -o pipefail
+client_id=$GITHUB_APP_ID # Client ID as first argument
 
-# JWTの有効期限（10分）
-CURRENT_TIME=$(date +%s)
-EXPIRATION_TIME=$(($CURRENT_TIME + 600))
+pem=$( cat $PEM_KEY ) 
 
-# JWTのヘッダーとペイロードを作成
-HEADER=$(printf '{"alg":"RS256","typ":"JWT"}' | openssl base64 -A | tr -d '=' | tr '/+' '_-' | tr -d '\n')
-PAYLOAD=$(printf '{"iat":%s,"exp":%s,"iss":"%s"}' "$CURRENT_TIME" "$EXPIRATION_TIME" "$GITHUB_APP_ID" | openssl base64 -A | tr -d '=' | tr '/+' '_-' | tr -d '\n')
+now=$(date +%s)
+iat=$((${now} - 60)) # Issues 60 seconds in the past
+exp=$((${now} + 600)) # Expires 10 minutes in the future
 
-# データを署名する
-HEADER_PAYLOAD="$HEADER.$PAYLOAD"
-SIGNATURE=$(printf '%s' "$HEADER_PAYLOAD" | openssl dgst -sha256 -sign ./github_app_private_key.pem | openssl base64 -A | tr -d '=' | tr '/+' '_-' | tr -d '\n')
+b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
 
-# 完全なJWTトークンを構築する
-$jwt="$HEADER_PAYLOAD.$SIGNATURE"
+header_json='{
+    "typ":"JWT",
+    "alg":"RS256"
+}'
+# Header encode
+header=$( echo -n "${header_json}" | b64enc )
 
-echo "Generated JWT: $jwt"
+payload_json='{
+    "iat":'"${iat}"',
+    "exp":'"${exp}"',
+    "iss":'"${client_id}"'
+}'
+# Payload encode
+payload=$( echo -n "${payload_json}" | b64enc )
+
+# Signature
+header_payload="${header}"."${payload}"
+signature=$(
+    openssl dgst -sha256 -sign ./github_app_private_key.pem \
+    <(echo -n "${header_payload}") | b64enc
+)
+
+# Create JWT
+jwt="${header_payload}"."${signature}"
+printf '%s\n' "JWT: $jwt"
 
 installation_id="$(curl --location --silent --request GET \
   --url "https://api.github.com/users/$GITHUB_OWNER/installation" \
@@ -81,39 +100,5 @@ registration_token="$(curl -X POST -fsSL \
 echo "registration_token is: $registration_token"
 echo "url is: https://github.com/$GITHUB_OWNER/$GITHUB_REPO"
 
-# registration_token2="$(curl -X POST -fsSL \
-#   -H 'Accept: application/vnd.github.v3+json' \
-#   -H "Authorization: Bearer $token" \
-#   -H 'X-GitHub-Api-Version: 2022-11-28' \
-#   "https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO2/actions/runners/registration-token" \
-#   | jq -r '.token')"
-# echo "registration_token is: $registration_token2"
-# echo "url is: https://github.com/$GITHUB_OWNER/$GITHUB_REPO2"
-
 # 取得したトークンでGithubリポジトリへアクセスします
 ./config.sh --url https://github.com/$GITHUB_OWNER/$GITHUB_REPO --token $registration_token --unattended --ephemeral && ./run.sh
-# ./config.sh --url https://github.com/$GITHUB_OWNER/$GITHUB_REPO2 --token $registration_token2 --unattended --ephemeral && ./run.sh
-
-# ',' を区切り文字として、環境変数を配列に分割
-# echo "REPOS is: $REPOS"
-# IFS=',' read -r -a repos_array <<< "$REPOS"
-
-# echo "repos_array is: $repos_array"
-
-# # 配列の要素数分だけループ
-# for repo in "${repos_array[@]}"
-# do
-#     echo "Current variable: $repo"
-#     # ここに環境変数ごとに実行したい処理を記述
-#     registration_token="$(curl -X POST -fsSL \
-#       -H 'Accept: application/vnd.github.v3+json' \
-#       -H "Authorization: Bearer $token" \
-#       -H 'X-GitHub-Api-Version: 2022-11-28' \
-#       "https://api.github.com/repos/$GITHUB_OWNER/$repo/actions/runners/registration-token" \
-#       | jq -r '.token')"
-    
-#     echo "registration_token is: $registration_token"
-#     echo "url is: https://github.com/$GITHUB_OWNER/$repo"
-#     # 取得したトークンでGithubリポジトリへアクセスします
-#     ./config.sh --url https://github.com/$GITHUB_OWNER/$repo --token $registration_token --unattended --ephemeral && ./run.sh
-# done
